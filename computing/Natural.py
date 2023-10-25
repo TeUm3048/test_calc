f3d5039c5370a0923565bd6cbbb717775a77667b
DoAlvaro <101565798+DoAlvaro@users.noreply.github.com>
2023-10-26 02:56:17 +0300

    Заглушки (#8)
    * Operators done
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Changes
    
    * ЗАГЛУШКИ

--- computing/Natural.py
+++ computing/Natural.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 from typing import Literal
+import math
+
 
 Digit = Literal[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
@@ -14,6 +16,9 @@ class Natural:
 
     def __len__(self):
         return len(self.data)
+
+    def __int__(self):
+        return int(''.join(list(map(str, self.data)))[::-1])
 
     def __lt__(self, other: Natural) -> bool:
         if len(self) > len(other):
@@ -66,5 +71,63 @@ class Natural:
     def is_not_zero(self) -> bool:
         return not (len(self) == 1 and self.data[0] == 0)
 
+    def increment(self) -> None:
+        # self.data = Natural(str(int(self)+1)).data
+        # РАСКОММЕНТИТЬ ЕСЛИ ПОНАДОБИТСЯ А НИЖЕ НЕ НАПИСАН КОД
+        pass
+
+    def add(self, other: Natural) -> Natural:
+        # return Natural(str(int(self)+int(other)))
+        # РАСКОММЕНТИТЬ ЕСЛИ ПОНАДОБИТСЯ А НИЖЕ НЕ НАПИСАН КОД
+        pass
+
+    def subtract(self, other: Natural) -> Natural:
+        # return Natural(str(int(self)-int(other)))
+        # РАСКОММЕНТИТЬ ЕСЛИ ПОНАДОБИТСЯ А НИЖЕ НЕ НАПИСАН КОД
+        # НЕ ВЫЧИТАТЬ ИЗ МЕНЬШЕГО БОЛЬШЕЕ
+        pass
+
+    def multiply_by_digit(self, other: int) -> Natural:
+        # return Natural(str(int(self)*other))
+        # РАСКОММЕНТИТЬ ЕСЛИ ПОНАДОБИТСЯ А НИЖЕ НЕ НАПИСАН КОД
+        pass
+
+    def multiply_by_power_of_10(self, k: int) -> Natural:
+        # return Natural(str(int(self)*(10**k)))
+        # РАСКОММЕНТИТЬ ЕСЛИ ПОНАДОБИТСЯ А НИЖЕ НЕ НАПИСАН КОД
+        pass
+
+    def multiply(self, other: Natural) -> Natural:
+        # return Natural(str(int(self)*int(other)))
+        # РАСКОММЕНТИТЬ ЕСЛИ ПОНАДОБИТСЯ А НИЖЕ НЕ НАПИСАН КОД
+        pass
+
+    def subtract_product_from_natural(self, other: Natural, k: int) -> Natural:
+        # return Natural(str(int(self)- k * int(other)))
+        # РАСКОММЕНТИТЬ ЕСЛИ ПОНАДОБИТСЯ А НИЖЕ НЕ НАПИСАН КОД
+        pass
+
+    # def get_digit_of_division_with_power(..):
+    # я сам не понял че надо так шо надо будет подумать
+
+    def div(self, other: Natural) -> Natural:
+        # return Natural(str(int(self)//int(other)))
+        # РАСКОММЕНТИТЬ ЕСЛИ ПОНАДОБИТСЯ А НИЖЕ НЕ НАПИСАН КОД
+        pass
+
+    def mod(self, other: Natural) -> Natural:
+        # return Natural(str(int(self)%int(other)))
+        # РАСКОММЕНТИТЬ ЕСЛИ ПОНАДОБИТСЯ А НИЖЕ НЕ НАПИСАН КОД
+        pass
+
+    def gcd(self, other: Natural) -> Natural:
+        # return Natural(str(math.gcd(int(self), int(other))))
+        # РАСКОММЕНТИТЬ ЕСЛИ ПОНАДОБИТСЯ А НИЖЕ НЕ НАПИСАН КОД
+        pass
+
+    def lcm(self, other: Natural) -> Natural:
+        # return Natural(str(math.lcm(int(self), int(other))))
+        # РАСКОММЕНТИТЬ ЕСЛИ ПОНАДОБИТСЯ А НИЖЕ НЕ НАПИСАН КОД
+        pass
     def __str__(self):
         return "".join(str(digit) for digit in self.data[::-1])
