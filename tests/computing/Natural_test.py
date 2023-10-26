fdf361a1bda8ee2ce6b5408a4ad2b116d5670252
Ivan Ryzhikov <52576454+TeUm3048@users.noreply.github.com>
2023-10-26 18:15:09 +0300

    Add compare test (#9)

--- tests/computing/Natural_test.py
+++ tests/computing/Natural_test.py
@@ -1,6 +1,24 @@
 from computing.Natural import Natural
 
 
+def test_compare_less():
+    a = Natural("6")
+    b = Natural("5555")
+    assert a.compare(b) == 1
+
+
+def test_compare_more():
+    a = Natural("666")
+    b = Natural("7")
+    assert a.compare(b) == 2
+
+
+def test_compare_equal():
+    a = Natural("17")
+    b = Natural("17")
+    assert a.compare(b) == 0
+
+
 def test_div_1():
     assert Natural("4") // Natural("2") == str(Natural("2"))
 
