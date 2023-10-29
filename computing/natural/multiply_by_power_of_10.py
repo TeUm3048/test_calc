bcada50dbd365a3750f2dd8d9eaf4071c6e03642
SaveliyKrivov <112757482+SaveliyKrivov@users.noreply.github.com>
2023-10-29 21:29:29 +0300

    mult_by_pow_of_10 zero case fixed (#34)

--- computing/natural/multiply_by_power_of_10.py
+++ computing/natural/multiply_by_power_of_10.py
@@ -5,6 +5,8 @@ from .Natural import Natural
 
 
 def multiply_by_power_of_10(num: Natural, k: Natural) -> Natural:
+    if not num.is_not_zero():
+        return num
     number = num.copy()
     times = k.copy()
     number.data.reverse()
