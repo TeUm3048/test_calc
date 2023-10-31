164ee07cb8eff2a28f528680db092204a0fe343e
Ivan Ryzhikov <52576454+TeUm3048@users.noreply.github.com>
2023-10-31 23:10:14 +0300

    Update div.py

--- computing/integer/div.py
+++ computing/integer/div.py
@@ -1,10 +1,6 @@
 # Модуль: DIV_ZZ_Z
 # Автор: Мавликаев_Иван_2381
-
-
 from .Integer import Integer
-
-
 def div(num1: Integer, num2: Integer) -> Integer:
     if not num2.number.is_not_zero():
         raise ZeroDivisionError
@@ -13,11 +9,11 @@ def div(num1: Integer, num2: Integer) -> Integer:
     res = Integer(num1.number.div(num2.number))
     if num1.sign < 0 and num2.sign > 0:
         res.sign = -1
-        if num1.number != num2.number.multiply(res.number):
+        if num1.number!=num2.number*res.number:
             res.number = res.number.add(Natural("1"))
     if num1.sign < 0 and num2.sign < 0:
         if num1.number != num2.number.multiply(res.number):
             res.number = res.number.add(Natural("1"))
-    if num1.sign > 0 and num2.sign < 0:
+    if num1.sign>0 and num2.sign<0:
         res.sign = -1
     return res
